d41faf8e1cc4a6233baead737c415aa297830443
wins <win4709hi@gmail.com>
2022-05-09 23:00:07 +0800

    feat: add routeWrapper, use it and no need to try catch error

---
+++ utils/routerWrapper.js
@@ -0,0 +1,43 @@
+const methodList = ['get', 'post', 'put', 'delete', 'patch', 'options']
+
+module.exports = function() {
+  // 需要在導入某檔案時才載入express，不然會有非預期問題
+  const express = require('express');
+  const router = express.Router();
+
+  /**
+   * 重新包裝過的router，可使用router所有http method。
+   * 在路由檔案導出 : module.exports = router.instance
+   */
+  const routerWrapper = {
+    instance: router
+  }
+
+  // 為routerWrapper註冊每個method
+  methodList.forEach(method => {
+    routerWrapper[method] = wrappingMethod(method)
+  })
+
+  function wrappingMethod(method) {
+    return function (...theArgs) {
+      // 參數只有一個的情況，和express預設一樣
+      if (theArgs.length === 1) {
+        return router[method](...theArgs)
+      }
+
+      const handler = theArgs.pop()
+      // 自定義req res function使用前，先包一層來catch錯誤
+      return router[method](...theArgs, handleErrorAsync(handler))
+    }
+  }
+
+  return routerWrapper
+}
+
+function handleErrorAsync(handler) {
+  return function (req, res, next) {
+    handler(req, res, next).catch(error => {
+      next(error)
+    })
+  }
+}
