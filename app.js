94740c96d2a04c960aa436e3c376238a04b1b68e
wins <win4709hi@gmail.com>
2022-05-11 11:36:50 +0800

    feat: listen all dumb mistake and catch it

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('./tools/processMonitor');
+
 const express = require('express');
 const path = require('path');
 const cors = require('cors')
---
+++ tools/processMonitor.js
@@ -0,0 +1,10 @@
+process.on('uncaughtException', err => {
+  console.log('---------------')
+	console.error('Uncaughted Exception！')
+	console.error(err);
+	process.exit(1);
+});
+
+process.on('unhandledRejection', (err, promise) => {
+  console.error('沒有catch：', promise, '原因：', err);
+});
