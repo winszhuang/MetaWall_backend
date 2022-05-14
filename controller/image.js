b76407afe03b32626d751b1015657ee3de19a3dc
wins <win4709hi@gmail.com>
2022-05-14 23:50:09 +0800

    feat: 補上基本防呆

--- controller/image.js
+++ controller/image.js
@@ -1,22 +1,26 @@
 const { Readable } = require('stream');
-const successHandler = require('../utils/successHandler');
 const { uploadFile, getFileAsync } = require('../store/s3');
+const successHandler = require('../utils/successHandler');
 const appError = require('../utils/appError');
 
 async function getImage(req, res, next) {
   if (!req.params.key) return appError('404', 'fetch image requires a key!!', next);
 
   const { key } = req.params;
+  if (!key) return appError('404', 'key required!!', next);
+
   const buffer = (await getFileAsync(key)).Body;
 
   return Readable.from(buffer).pipe(res);
 }
 
-async function postImage(req, res) {
+async function postImage(req, res, next) {
   const { file } = req;
+  if (!file) return appError('404', 'file required!!', next);
+
   const result = await uploadFile(file);
 
-  successHandler(res, 200, {
+  return successHandler(res, 200, {
     imageUrl: `/images/${result.Key}`,
   });
 }
