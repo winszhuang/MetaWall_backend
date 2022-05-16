1e0a00c10bb995727fb44508e80dfab56a5de1cf
wins <win4709hi@gmail.com>
2022-05-16 17:28:19 +0800

    feat: RequestBlocker增加可處理非同步驗證function的情況

--- services/blocker/RequestBlocker.js
+++ services/blocker/RequestBlocker.js
@@ -1,5 +1,34 @@
 /* eslint-disable no-restricted-syntax */
 
+/**
+ * @example
+ * function postController(req, res, next) {
+    const errorData = new RequestBlocker(req)
+      .setBlocker(
+        isFileExist, // 自定義驗證錯誤function，可參考image-rule.js
+        isFieldNameImage,
+      ).getErrorData();
+
+    if (errorData) {
+      const { statusCode, errorMessage } = errorData;
+      return appError(statusCode, errorMessage, next);
+    }
+    ...
+
+  如果setBlocker中帶入的驗證function有"非同步的"，需要在new之前加上await，並且更改為使用getErrorDataAsync方法
+  async function postController(req, res, next) {
+    const errorData = await new RequestBlocker(req)
+      .setBlocker(
+        isFileExistAsync, // async function
+        isFieldNameImage,
+      ).getErrorDataAsync();
+
+    if (errorData) {
+      const { statusCode, errorMessage } = errorData;
+      return appError(statusCode, errorMessage, next);
+    }
+    ...
+ */
 class RequestBlocker {
   constructor(request) {
     this.request = request;
@@ -20,6 +49,37 @@ class RequestBlocker {
 
     return null;
   }
+
+  async getErrorDataAsync() {
+    // eslint-disable-next-line guard-for-in
+    for (const blockerFunc of this.strategyList) {
+      /** result值可能為true、errorData、或者promise */
+      const result = blockerFunc(this.request);
+      if (isPromise(result)) {
+        // eslint-disable-next-line no-await-in-loop
+        const innerResult = await newPromise(result);
+        if (innerResult !== true) return innerResult;
+      } else if (result !== true) return result;
+    }
+
+    return null;
+  }
+}
+
+function isPromise(data) {
+  return typeof data.catch === 'function'
+  && data[Symbol.toStringTag] === 'Promise';
+}
+
+function newPromise(promise) {
+  return new Promise((resolve, reject) => {
+    promise.then((data) => {
+      resolve(data);
+    })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 }
 
 module.exports = RequestBlocker;
