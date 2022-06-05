8c4f2705dec5927c61f217689e8058fa70618cd9
wins <win4709hi@gmail.com>
2022-06-05 15:27:46 +0800

    fix: get image no need auth

--- routes/images.js
+++ routes/images.js
@@ -5,7 +5,7 @@ const isAuth = require('../middleware/isAuth');
 const upload = multer({ dest: 'uploads/' });
 const { getImage, postImage } = require('../controller/image');
 
-router.get('/:key', isAuth, getImage);
+router.get('/:key', getImage);
 router.post('/', isAuth, upload.single('image'), postImage);
 
 module.exports = router.instance;
